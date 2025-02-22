4271c09a8fd26d879da53862ea78d171a3638e72
Nahida Bashir <nahidabashir08@gmail.com>
2025-02-22 21:43:05 +0530

    Added base project setup for User Microservice.

---
+++ UserMicroservices/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace UserMicroservice.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; } 
+        public string Role { get; set; }
+        public ICollection<Guid> OrderIds { get; set; }  
+        public ICollection<Guid> AddressIds { get; set; }
+        public Guid? PaymentId { get; set; }  
+        public Guid? CartId { get; set; }
+    }
+}
