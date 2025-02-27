714dfa87e58b7023c4d01afffe66253ef2d9a9dc
KiranmayaPuhan6 <kiranmayapuhan6@gmail.com>
2025-02-27 08:06:09 +0530

    Added generic repository for crud operations

---
+++ AppDbContext/DBContext/GenericAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace AppDbContext.DBContext
+{
+    public class GenericDbContext<T> : DbContext where T : class
+    {
+        public GenericDbContext(DbContextOptions<GenericDbContext<T>> options) : base(options) { }
+
+        public DbSet<T> Entities => Set<T>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
